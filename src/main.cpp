a7d8f8f8888e874ac5f6680b97cf61d9c318a928
Ch-Tima <chizhevskii.tima@gmail.com>
2025-08-09 19:51:34 +0200

    Add stringToUint16t

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,29 @@
 
 using namespace std;
 
+#define OK 0
+#define INVALID_ARG 1
+#define OUT_OF_RANGE 2
+
+struct to_unit16t
+{
+    uint16_t value = 0;
+    uint8_t status = 0;
+};
+
+
+to_unit16t stringToUint16t(string s){
+    to_unit16t r;
+    try{
+        r.value = std::stoi(s);
+    }catch(std::invalid_argument const& ex){
+        r.status = INVALID_ARG;//Bad Request
+    }catch (std::out_of_range const& ex){
+        r.status = INVALID_ARG;//Range Not Satisfiable
+    }
+    return r;
+}
+
 int main(){
     string line;
     uint8_t take = UINT8_MAX;
@@ -15,7 +38,13 @@ int main(){
     ifstream max_br_file("/sys/class/backlight/amdgpu_bl1/max_brightness");
     if(max_br_file.is_open()){
         while (getline(max_br_file, line)){
-            max_br = std::stoi(line);
+            to_unit16t r = stringToUint16t(line);
+            if(r.status == OK)
+                max_br = r.value;
+            else {
+                cout << "ERROR: " << r.status << " set default value in max_br to" << UINT16_MAX << '\n';
+                max_br = UINT16_MAX;
+            }
         }
         max_br_file.clear();
     }
@@ -26,8 +55,15 @@ int main(){
         ifstream mfile("/sys/bus/iio/devices/iio:device0/in_illuminance_raw");
         if(mfile.is_open()){
             while(getline(mfile, line)){
-                il_value = std::stoi(line);
-                cout << il_value << '\n';
+                to_unit16t r = stringToUint16t(line);
+                if(r.status == OK){
+                    il_value = r.value;
+                    cout << il_value << '\n';
+                }else {
+                    cout << "ERROR: " << r.status << " set default value in il_value to 0" << '\n';
+                    il_value = 0;
+                }
+                
             }
             mfile.close();
         }
@@ -35,5 +71,5 @@ int main(){
         take--;
         this_thread::sleep_for(chrono::milliseconds(1000));
     }
-    return 0;
+    return OK;
 }
