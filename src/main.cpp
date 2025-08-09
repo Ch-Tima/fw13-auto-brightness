8ceb488383a4b5653b0e9bfc163404362ff7fb1a
Ch-Tima <chizhevskii.tima@gmail.com>
2025-08-09 19:22:41 +0200

    Read max_brughtness

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,26 @@ using namespace std;
 
 int main(){
     string line;
-    short take = 256;
-    short value = 0;
+    uint8_t take = UINT8_MAX;
+    uint16_t il_value = 0;
+    uint16_t max_br = 0;
+
+    ifstream max_br_file("/sys/class/backlight/amdgpu_bl1/max_brightness");
+    if(max_br_file.is_open()){
+        while (getline(max_br_file, line)){
+            max_br = std::stoi(line);
+        }
+        max_br_file.clear();
+    }
+
+    cout << "max_brightness: " << max_br << '\n'; 
+
     while(take > 1){
         ifstream mfile("/sys/bus/iio/devices/iio:device0/in_illuminance_raw");
         if(mfile.is_open()){
             while(getline(mfile, line)){
-                value = std::stoi(line);
-                cout << value*100 << '\n';
+                il_value = std::stoi(line);
+                cout << il_value << '\n';
             }
             mfile.close();
         }
