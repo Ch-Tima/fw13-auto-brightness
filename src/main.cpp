269e7838a629739d38d0832775a5fa481f671893
Ch-Tima <chizhevskii.tima@gmail.com>
2025-08-13 12:56:03 +0200

    Beta control brightness

--- src/main.cpp
+++ src/main.cpp
@@ -43,36 +43,61 @@ struct vec2_u32
     double y;
 };
 
-int main(){
-
-    uint8_t size = 4;
+uint16_t cal(double mX){
+    uint8_t size = 5;
     vec2_u32 v[size] = {
-        { 0.0, 0.0 }, 
-        { 50, 32768 },
-        { 600, 55705 },
-        { 3355, 65535 },
+        { 0.0, 12000 }, 
+        { 50, 20000 }, 
+        { 180, 40000 },
+        { 1400, 55000 },
+        { 3355, UINT16_MAX },
+
+        // { 0, 122 }, 
+        // { 30, 195 },
+        // { 50,  391 },
+        // { 114, 415},
+        // { 335, 647 },
     };
 
-    double mX = 49;
     double nowY = 0;
     double m = 0;
 
     for(int i = 1; i < size; i++){
         if(mX > v[i-1].x && mX <= v[i].x){
-            cout << v[i-1].x << " | " << v[i].x << '\n';
+            //cout << v[i-1].x << " | " << v[i].x << '\n';
             m = (v[i].y - v[i-1].y)/(v[i].x - v[i-1].x);
-            cout << "m: " << m << '\n';
-            nowY = m*mX-v[i-1].x;
-            cout << "nowY: " << nowY << '\n';
-            break;
+            //cout << "m: " << m << '\n';
+            nowY = v[i-1].y + m * (mX - v[i-1].x);
+            //cout << "nowY: " << nowY << '\n';
+            return nowY;
         }
-        cout << "i" << i << '\n';
+        //cout << "i" << i << '\n';
     }
 
-    return OK;
+    if (mX < v[0].x)  return static_cast<uint16_t>(v[0].y);
+    if (mX > v[size-1].x) return static_cast<uint16_t>(v[size-1].y);
+
+    return 0;
+}
+
+int main(){
+
+
+    cout << cal(2) << '\n';
+    cout << cal(350) << '\n';
+    cout << cal(800) << '\n';
+    cout << cal(1000) << '\n';
+    cout << cal(2000) << '\n';
+
+    //return OK;
+
 
     string line;
     uint8_t take = UINT8_MAX;
+    uint8_t new_limit = 50;
+    uint8_t number_of_check = 3;
+    uint8_t count_check = 0;
+    uint16_t old_value = UINT16_MAX;// Illuminance sensor old value
     uint16_t il_value = 0;// Illuminance sensor value
     uint16_t max_br = 0;// Maximum brightness value
 
@@ -92,6 +117,7 @@ int main(){
     cout << "max_brightness: " << max_br << '\n'; 
 
     while(take > 1){// Loop to periodically read illuminance sensor value
+        //cout << take << ")\n";
         ifstream mfile("/sys/bus/iio/devices/iio:device0/in_illuminance_raw");
         if(mfile.is_open()){
             getline(mfile, line);
@@ -99,15 +125,42 @@ int main(){
             to_unit16t r = stringToUint16t(line);
             if(r.status == OK){
                 il_value = r.value;
-                cout << il_value << '\n';// Print illuminance value
+                cout << "illuminance value: " << il_value << '\n';// Print illuminance value
+                cout << "cal(il_value): " << cal(il_value) << '\n';
             }else {
                 cout << "ERROR: " << r.status << " set default value in il_value to 0" << '\n';
                 il_value = 0;
             }
         }
         else cout << "Unable to open file\n";
+
+        if(il_value > old_value+new_limit || il_value < old_value-new_limit){
+            if(count_check >= number_of_check){
+                cout << "new!\n";
+                old_value = il_value;
+
+                ofstream br_file("/sys/class/backlight/amdgpu_bl1/brightness",
+                    std::ios::out | std::ios::trunc);
+                if(br_file.is_open()){
+                    br_file << cal(il_value);
+                    br_file.close();
+                }else{
+                    cout << "ERR OPEN SCBAB\n";
+                }
+
+                count_check = 0;
+            }else {
+                count_check++;
+            }
+
+        }else { 
+            cout << "old!\n";
+        }
+
         take--;
-        this_thread::sleep_for(chrono::milliseconds(1000));// Wait 1 second before next read
+
+        cout << "\n";
+        this_thread::sleep_for(chrono::milliseconds(500));// Wait 0.5 second before next read
     }
     return OK;
 }
