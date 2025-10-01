63bd2a430f3f26c929fdcb22da619158a7e10c85
Ch-Tima <chizhevskii.tima@gmail.com>
2025-10-01 19:23:23 +0200

    Add LICENSE

--- README.md
+++ README.md
@@ -84,3 +84,5 @@ PR и баг-репорты приветствуются ;)
 - **GitHub**: [Ch-Tima](https://github.com/Ch-Tima)
 
 ## License
+This project is licensed under the GNU Affero General Public License v3.0 (AGPL-3.0)  
+See the [LICENSE](LICENSE) file for details.
--- ui/src/view/MainView.cpp
+++ ui/src/view/MainView.cpp
@@ -1,5 +1,19 @@
-#include "view/MainView.h"
+/*
+ * fw13-auto-brightness
+ * Part of fw13-auto-brightness project (AutoBrightnessUI / AutoBrightnessIluminance)
+ * Copyright (C) 2025  <Ch-Tima>
+ *
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
+#include "view/MainView.h"
 
 MainView::MainView(QWidget *parent) : QWidget(parent), uiBlocked(false){
     init();
